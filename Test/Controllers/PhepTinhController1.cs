3c8c683041e269387656eafdd9b22a7e0905ecab
LE NGOC BIEN <LE NGOC BIEN@LENGOCBIEN>
2024-05-21 21:53:19 +0700

    Add project files.

---
+++ Test/Controllers/PhepTinhController1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Controllers
+{
+    public class PhepTinhController1 : Controller
+    {
+        public IActionResult Index(double So1, double So2, string phepTinh)
+
+        {
+            double ketQua = 0;
+            switch (phepTinh)
+            {
+                case "+":
+                    ketQua = So1 + So2;
+                    break;
+
+                case "-":
+                    ketQua = So1 - So2;
+                    break;
+                case "*":
+                    ketQua = So1 * So2;
+                    break;
+                case "/":
+                    ketQua = So1 / So2;
+                    break;
+                    //default:
+                    //    break;
+            }
+            ViewData["K"] = ketQua;
+            ViewBag.KQ = ketQua;
+            return View();
+        }
+        //public IActionResult XuLy(double So1,double So2,string phepTinh)
+        //{
+        //    double ketQua=0;
+        //    switch (phepTinh)
+        //    {
+        //        case "+":ketQua = So1 + So2;
+        //            break;
+
+        //        case"-":ketQua = So1 - So2;
+        //            break;
+        //        case "*":
+        //            ketQua = So1 * So2;
+        //            break;
+        //        case "/":
+        //            ketQua = So1 / So2;
+        //            break;
+        //        //default:
+                
+        //        //    break;
+        //    }
+        //    ViewData["K"] = ketQua;
+        //    ViewBag.KQ =ketQua;
+        //    return View();
+        //}
+    }
+}
